c1b201c7ed6a039f09aa418e3650e92e97dfda7e
fengjinlong <18618166564@163.com>
2019-01-08 12:26:38 +0800

    ad

---
+++ demo/src/components/HelloWorld2.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <CC></CC>
+  </div>
+</template>
+
+<script>
+import CC from './checkbox/Checkbox'
+export default {
+  name: 'HelloWorld2',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    CC
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ demo/src/components/checkbox/Checkbox.vue
@@ -0,0 +1,69 @@
+<template>
+  <label>
+    <span>
+      <input
+        type="checkbox"
+        :disabled="disabled"
+        :checked="currentValue"
+        @change="change">
+    </span>
+    <slot></slot>
+  </label>
+</template>
+<script>
+  import Emitter from '../../mixins/emitter.js';
+  export default {
+    mixins: [ Emitter ],
+    name: 'iCheckbox',
+    props: {
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      value: {
+        type: [String, Number, Boolean],
+        default: false
+      },
+      trueValue: {
+        type: [String, Number, Boolean],
+        default: true
+      },
+      falseValue: {
+        type: [String, Number, Boolean],
+        default: false
+      }
+    },
+    data () {
+      return {
+        currentValue: this.value
+      };
+    },
+    methods: {
+      change (event) {
+        if (this.disabled) {
+          return false;
+        }
+
+        const checked = event.target.checked;
+        this.currentValue = checked;
+
+        const value = checked ? this.trueValue : this.falseValue
+        this.$emit('input', value);
+        this.$emit('on-change', value);
+        this.dispatch('iFormItem', 'on-form-change', value);
+      },
+      updateModel () {
+        this.currentValue = this.value === this.trueValue;
+      }
+    },
+    watch: {
+      value (val) {
+        if (val === this.trueValue || val === this.falseValue) {
+          this.updateModel();
+        } else {
+          throw 'Value should be trueValue or falseValue.';
+        }
+      }
+    }
+  }
+</script>
