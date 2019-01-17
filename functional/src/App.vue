4d2c2afed17d74ac1d9657bb91814acb64aa71dc
fengjinlong <18618166564@163.com>
2019-01-17 20:28:22 +0800

    a

--- functional/src/App.vue
+++ functional/src/App.vue
@@ -1,46 +1,23 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    
+  <div>
+    <myComponent :render="render"></myComponent>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import myComponent from './myComponent.vue';
+  
+  export default {
+    components: { myComponent },
+    data () {
+      return {
+        render: (h) => {
+          return h('div', {
+            style: {
+              color: 'red'
+            }
+          }, '自定义内容');
+        }
+      }
     }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
---
+++ functional/src/myComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Render :render="render"></Render>
+  </div>
+</template>
+<script>
+  import Render from './render.js'
+  export default {
+    components: { Render },
+    props: {
+      render: Function
+    }
+  }
+</script>
